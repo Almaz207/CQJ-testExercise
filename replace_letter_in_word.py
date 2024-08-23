dac20df1ee1baa561829e9c7b915666a3feef69b
Almaz207 <muzafarov.almaz2012@yandex.ru>
2024-08-23 23:45:44 +0400

    add finally version program

--- replace_letter_in_word.py
+++ replace_letter_in_word.py
@@ -12,13 +12,11 @@ class Word:
             instruction = file.readlines()
             list_instruction = [record.replace('\n', '') for record in instruction]
             self.instruction = list_instruction
-            print(self.instruction)
 
         with open(file_word, 'r') as file:
             words = file.readlines()
             list_word = [word.replace('\n', '') for word in words]
             self.words = list_word
-            print(self.words)
 
     def replace_letter(self):
         new_words = {}
@@ -28,8 +26,13 @@ class Word:
                 quantity = quantity + word.count(record.split('=')[0])
                 word = word.replace(record.split('=')[0], record.split('=')[1], -1)
             new_words[word] = quantity
-        return new_words
+        new_words = sorted(new_words.items(), key=lambda item: item[1], reverse=True)
+        list_words = [pair[0] for pair in new_words]
+        return list_words
 
+instruction = input("Введите имя файла с инструкциями по замене: ")
+list_word = input("Введите имя файла со списком слов в которых нужно заменить буквы или их сочетания: ")
+result = Word(instruction, list_word).replace_letter()
+for word in result:
+    print(word)
 
-zapros = Word('instruction.txt', 'word.txt').replace_letter()
-print(zapros.sorted())
